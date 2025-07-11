4a0f1360fa4a092f278e67bac5820b869573230e
ThainaraM <149270370+ThainaraM@users.noreply.github.com>
2025-07-11 20:55:42 -0300

    Create UsuarioService.java

---
+++ src/main/java/com/orangejuice/bank/service/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.orangejuice.bank.service;
+
+import com.orangejuice.bank.model.Usuario;
+import com.orangejuice.bank.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public Usuario criar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    public List<Usuario> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Usuario buscarPorId(UUID id) {
+        return repository.findById(id).orElse(null);
+    }
+}
+
