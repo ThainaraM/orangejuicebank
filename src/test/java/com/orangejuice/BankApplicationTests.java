fab5a2657d3e2945e2a6d940865571ae5e4f9f25
ThainaraM <149270370+ThainaraM@users.noreply.github.com>
2025-07-12 18:54:03 -0300

    commit

--- src/test/java/com/orangejuice/BankApplicationTests.java
+++ src/test/java/com/orangejuice/BankApplicationTests.java
@@ -1,4 +1,4 @@
-package orangejuicebank;
+package com.orangejuice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
